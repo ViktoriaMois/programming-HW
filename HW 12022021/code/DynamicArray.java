2139acfcdccaf053cbaae188991134b6f6520826
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-02-25 23:59:01 +0300

    Throws Exception
    end of the process

--- HW 12022021/code/DynamicArray.java
+++ HW 12022021/code/DynamicArray.java
@@ -5,9 +5,21 @@ public class DynamicArray <A> {
     private int DEFAULT_SIZE = 10;
     private A[] Array;
 
+    public DynamicArray () {
 
     }
 
-    
+
+    }
+
+    public DynamicArray(int start_size){
+        if (start_size > -1)
+            Array =  (A[]) new Object[start_size];
+        else
+            throwsException();
+
+    }
+
+
 
 }
