435667c3635cd398e76d5f673adbd4600b8b3c17
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-03 00:30:47 +0300

    Create DoubleLinkedList.java
    Made new class DoubleLinkedList.
    Added 2 elements: head(begin) and tale(end). Initialisation head and tale.

---
+++ HW26022021/code/DoubleLinkedList.java
@@ -0,0 +1,10 @@
+public class DoubleLinkedList<L> {
+
+    private DoubleLinkedList<L> head;
+    private DoubleLinkedList<L> tale;
+
+    public DoubleLinkedList() {
+
+        head = null;
+        tale = null;
+
