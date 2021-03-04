587abbbc2aa08d073b57b0bcd0b6d608ffdf4aef
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-04 17:41:41 +0300

    Update DoubleLinkedListElement.java

--- HW26022021/code/DoubleLinkedListElement.java
+++ HW26022021/code/DoubleLinkedListElement.java
@@ -6,7 +6,7 @@ L data;
 DoubleLinkedListElement<L> next;
 DoubleLinkedListElement<L> prev;
 
-public DoubleLinkedListElement (DoubleLinkedListElement<L> prev, L data, DoubleLinkedListElement<L> next){
+public DoubleLinkedListElement(L data, DoubleLinkedListElement<L> next, DoubleLinkedListElement<L> prev){
     this.data = data;
     this.next = next;
     this.prev = prev;
