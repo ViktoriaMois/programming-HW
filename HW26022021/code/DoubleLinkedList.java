ae62c3c055d35ad2e59ccb8aea424b6fa7982e97
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-04 17:34:27 +0300

    add element before
    + method addBefore wich can add new element before element that exist in the list. If list is emty, we can't use this method.

--- HW26022021/code/DoubleLinkedList.java
+++ HW26022021/code/DoubleLinkedList.java
@@ -47,3 +47,20 @@ public class DoubleLinkedList<L> {
             throw new ArithmeticException();
     }
 
+    public void addBefore (L x, L y){
+        if (isEmpty())
+            throw new ArithmeticException(x.toString());
+        DoubleLinkedListElement<L> current = head;
+        while (current != null && !current.data.equals(x))
+            current = current.next;
+        if (current == null)
+            throw new ArithmeticException(x.toString());
+        DoubleLinkedListElement<L> tmp = new DoubleLinkedListElement<L>(current.prev, y, current);
+        if (current.prev != null)
+            current.prev.next = tmp;
+        else head = tmp;
+        current.prev = tmp;
+
+        size++;
+    }
+
