6485c16b2b2b3fc7a489106d1e311fe186af3a59
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-04 17:51:55 +0300

    TestGet
    test for method get

--- HW12022021/tests/DynamicArrayTests.java
+++ HW12022021/tests/DynamicArrayTests.java
@@ -6,5 +6,13 @@ import org.junit.jupiter.api.Test;
 
 public class DynamicArrayTests extends Assertions {
 
+    @Test
+    public void DynamicArray_get_throwsException(){
+        DynamicArray dynamicArray = new DynamicArray();
+        assertThrows(ArithmeticException.class, () -> dynamicArray.get(-1));
+        assertThrows(ArithmeticException.class, () -> dynamicArray.get(10));
+
+    }
+
 
 }
