2a18cd0fd653bcadce8e579df4066395e4fcc746
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-04 17:43:55 +0300

    Create DoubleLinkedListTest.java

---
+++ HW26022021/test/DoubleLinkedListTest.java
@@ -0,0 +1,9 @@
+package test;
+
+import code.DoubleLinkedList;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class DoubleLinkedListTest extends Assertions {
+
