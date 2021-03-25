abaebca0b196c137250a93095e140cb6bb7f3aec
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-25 13:38:49 +0300

    Update DynamicArrayTests.java

--- HW12022021/tests/DynamicArrayTests.java
+++ HW12022021/tests/DynamicArrayTests.java
@@ -25,10 +25,6 @@ public class DynamicArrayTests extends Assertions {
 //    }
 
     @Test
-    public void DynamicArray_get_throwsException(){
-        DynamicArray dynamicArray = new DynamicArray();
-        assertThrows(ArithmeticException.class, () -> dynamicArray.get(-1));
-        assertThrows(ArithmeticException.class, () -> dynamicArray.get(10));
 
     }
 
