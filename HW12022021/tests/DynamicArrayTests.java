82069ad5a82e8d4d390d6586d0c5f07f68e1c125
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-25 13:43:39 +0300

    Update DynamicArrayTests.java

--- HW12022021/tests/DynamicArrayTests.java
+++ HW12022021/tests/DynamicArrayTests.java
@@ -7,23 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Array;
 
 public class DynamicArrayTests extends Assertions {
-
-//    @Test
-//    public void DynamicArray_get_throwsException(){
-//        DynamicArray dynamicArray = new DynamicArray();
-//        assertThrows(ArithmeticException.class, () -> dynamicArray.getData(-1));
-//        assertThrows(ArithmeticException.class, () -> dynamicArray.getData(10));
-//
-//    }
-
-//    @Test
-//    public void DynamicArray_set_throwsException(){
-//        DynamicArray dynamicArray = new DynamicArray();
-//        assertThrows(ArithmeticException.class, () -> dynamicArray.setData(-1, 5));
-//        assertThrows(ArithmeticException.class, () -> dynamicArray.setData(10, 18));
-//
-//    }
-
+    
     @Test
     public void testGetSize(){
         DynamicArray array = new DynamicArray(10);
