963fba81999fe71234009e822488f752daa88fa1
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-25 17:25:30 +0300

    function init_test
    initilization entity of stack

--- HW12022021/stack/StackTest.java
+++ HW12022021/stack/StackTest.java
@@ -6,4 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StackTest {
 
+        @Test
+        public void init_test() {
+            Stack<Integer> stack = new Stack<Integer>();
+        }
+
     }
