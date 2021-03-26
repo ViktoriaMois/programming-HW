da3098e2bfeea73bc39a986e7a382c91ee6a11bd
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-26 13:47:19 +0300

    Task 7 complete
    create sort method "sort by search"

--- QuadraticSort/QuadraticSort.java
+++ QuadraticSort/QuadraticSort.java
@@ -2,4 +2,19 @@ import java.util.Arrays;
 
 public class QuadraticSort {
     public void sort(int[] data) {
+
+        for (int i = 0; i < data.length; i++) {
+            int min = data[i];
+            int imin = i;
+            for (int j = i + 1; j < data.length; j++){
+                if (data[j] < min){
+                    min = data[j];
+                    imin = j;
+                }
+            }
+            int N = data[i];
+            data[i] = min;
+            data[imin] = N;
+        }
+    }
 }
