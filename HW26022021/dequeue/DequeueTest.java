b5a4780e2d3ecb03d9f83c5ad31ac2c5c4823c16
ViktoriaMois <78952381+ViktoriaMois@users.noreply.github.com>
2021-03-26 03:24:55 +0300

    Test pushBack

--- HW26022021/dequeue/DequeueTest.java
+++ HW26022021/dequeue/DequeueTest.java
@@ -4,4 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DequeueTest {
+
+    @Test
+    public void testPushBack(){
+        Dequeue deq = new Dequeue();
+        deq.pushFront(2);
+        deq.pushBack(3);
+        assertEquals(3, deq.peekBack());
+    }
+
 }
