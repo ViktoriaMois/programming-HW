4cee4634d779208d625dcf1178ae781ceac8f6cc
ViktoriaMois <vika.moiseewa2002@gmail.com>
2021-04-05 02:23:10 +0300

    Function Sort
    prints the result after function mergeSort

--- NLogNSort/NLogNSort.java
+++ NLogNSort/NLogNSort.java
@@ -1,2 +1,11 @@
 public class NLogNSort {
+
+    public void sort(int[] data) {
+
+        mergeSort(data, 0, data.length - 1);
+
+        for (int i = 0; i < data.length; i++)
+            System.out.print(data[i] + " ");
+    }
+
 }
