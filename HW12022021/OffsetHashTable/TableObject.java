92e40a621e7536ff8691f55fe23abb8833224959
KramA1lce <vika.moiseewa2002@gmail.com>
2021-05-27 23:35:26 +0300

    TableObject Created
    TableObject is set

---
+++ HW12022021/OffsetHashTable/TableObject.java
@@ -0,0 +1,20 @@
+package OffsetHashTable;
+
+
+public class TableObject {
+    int key;
+    public int data;
+
+    public TableObject() {
+
+    }
+
+    public TableObject(int key, int data) {
+        this.key = key;
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+}
