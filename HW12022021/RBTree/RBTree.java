5e646a76b1bdec958038fe63977649a2f434c7bf
KramA1lce <vika.moiseewa2002@gmail.com>
2021-06-12 04:57:13 +0300

    Update RBTree.java
    Added getSuccessor, getPredecessor, search and additions to them

--- HW12022021/RBTree/RBTree.java
+++ HW12022021/RBTree/RBTree.java
@@ -61,4 +61,83 @@ public class RBTree {
 
         return y;
     }
+
+    boolean search(String key) {
+        root = searchRec(root, key);
+        return root != null;
+    }
+
+    public Node getSuccessor(Node root, String value) {
+        return successorRec(root, value);
+    }
+
+    public Node getPredecessor(Node root, String value) {
+        return predecessorRec(root, value);
+    }
+
+    public Node searchRec(Node root, String key) {
+        if (root == null || root.key.equals(key)) {
+        return root;
+        }
+        if (key.compareTo(root.key) <0) {
+            return searchRec(root.leftNode, key);
+        }
+        return  searchRec(root.rightNode, key);
+    }
+
+    public Node minValue(Node root) {
+        Node minVal = root;
+        while (root.leftNode != null) {
+            minVal = root.leftNode;
+            root = root.leftNode;
+        }
+        return minVal;
+    }
+
+    public Node maxValue(Node root) {
+        Node maxVal = root;
+        while (root.rightNode != null) {
+            maxVal = root.rightNode;
+            root = root.rightNode;
+        }
+        return maxVal;
+    }
+
+    public Node successorRec(Node root, String value) {
+        Node current = searchRec(root, value);
+        if (current == null) {
+            return current;
+        }
+
+        if (current.rightNode != null) {
+            return minValue(current.rightNode);
+        } else {
+            Node tmp = root.leftNode;
+            while (tmp.rightNode != null)
+                tmp = tmp.rightNode;
+            current = tmp;
+        }
+        return current;
+    }
+
+    public Node predecessorRec(Node root, String value) {
+        Node pre = null;
+        while (root != null) {
+            if (root.key.equals(value)) {
+                if (root.leftNode != null) {
+                    pre = root.leftNode;
+                    while (pre.leftNode != null)
+                        pre = pre.rightNode;
+                }
+                return pre;
+            }
+            else if (value.compareTo(root.key) > 0) {
+                    pre = root;
+            root = root.rightNode;
+            }
+        }
+        return pre;
+    }
+
+    
 }
